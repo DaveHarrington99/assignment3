c995b74a98726bac464eb17caaaa6619960754d8
dh26 <118359016@umail.ucc.ie>
2020-12-17 06:09:43 +0000

    second test

--- src/test/java/Assignment3/ColourTest.java
+++ src/test/java/Assignment3/ColourTest.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
     @Test
-    public void acceptColourIfMeetsRequirements(){
+    public void acceptColourIfObjectCreatesSuccessfully(){
         Colour colour1 = new Colour(40,80,-90);
-        assertTrue(colour1.satisfiesRequirements(), "Parameters");
+        colour1.satisfiesRequirements();
+    }
+
+    @Test
+    public void acceptIfObjectMeetsBoundaryRequirements(){
+        Colour colour1 = new Colour(40, 80, -90);
+        assertFalse(colour1.satisfiesRequirements());
     }
 }
