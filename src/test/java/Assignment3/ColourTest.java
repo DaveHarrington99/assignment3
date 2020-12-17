bde5a6966dce5f0d2421660889ecc97c10c7bcb1
dh26 <118359016@umail.ucc.ie>
2020-12-17 06:20:02 +0000

    third test part a

--- src/test/java/Assignment3/ColourTest.java
+++ src/test/java/Assignment3/ColourTest.java
@@ -14,6 +14,12 @@ class ColourTest {
     @Test
     public void acceptIfObjectMeetsBoundaryRequirements(){
         Colour colour1 = new Colour(40, 80, -90);
-        assertTrue(colour1.satisfiesRequirements(), "parameter values outside range");
+        colour1.satisfiesRequirements();
+    }
+
+    @Test
+    public void acceptIfDefaultModelIsRGB(){
+        Colour colour1 = new Colour(40, 80,90);
+        assertFalse(colour1.satisfiesRequirements(), "Not RGB by default");
     }
 }
